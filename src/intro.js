0e6d73ed41c20c6351f7250eed7efd9a642599cc
Homayoun <homayoun763@gmail.com>
2024-06-15 23:31:19 +0330

    factorial fun and test

--- src/intro.js
+++ src/intro.js
@@ -14,5 +14,11 @@ export function fizzBuzz(n) {
 export function calculateAverage(numbers) {
   if (numbers.length === 0) return NaN;
   const sum = numbers.reduce((sum, current) => sum + current, 0);
-  return sum / numbers.length
+  return sum / numbers.length;
+}
+
+export function factorial(n) {
+  if (n < 0) return undefined;
+  if (n === 0 || n === 1) return 1;
+  return n * factorial(n - 1);
 }
