853982a6c6da908b10040b178ba7040420d7f16c
Homayoun <homayoun763@gmail.com>
2024-07-31 14:02:22 +0330

    write test for validateUserInput

--- src/core.js
+++ src/core.js
@@ -30,15 +30,15 @@ export function calculateDiscount(price, discountCode) {
 export function validateUserInput(username, age) {
   let errors = [];
 
-  if (typeof username !== 'string' || username.length < 3) {
+  if (typeof username !== 'string' || username.length < 3 || username.length > 255) {
     errors.push('Invalid username');
   }
 
-  if (typeof age !== 'number' || age < 18) {
+  if (typeof age !== 'number' || age < 18 || age > 100) {
     errors.push('Invalid age');
   }
 
-  return errors.length === 0 ? 'Validation successful' : errors.join(', ');
+  return errors.length == 0 ? 'Validation successful' : errors.join(', ');
 }
 
 // Lesson: Boundary testing
--- tests/core.test.js
+++ tests/core.test.js
@@ -1,5 +1,5 @@
 import { it, expect, describe } from "vitest";
-import { calculateDiscount, getCoupons } from "../src/core";
+import { calculateDiscount, getCoupons, validateUserInput } from "../src/core";
 
 describe("getCoupons", () => {
   it("should return an arrays of coupons", () => {
@@ -47,6 +47,41 @@ describe("calculateDiscount", () => {
   });
 
   it("should handle invalid discount code", () => {
-    expect(calculateDiscount(10, "INVALIDE")).toBe(10)
+    expect(calculateDiscount(10, "INVALIDE")).toBe(10);
+  });
+});
+
+describe("validateUserInput", () => {
+  it("should return success if given valid input ", () => {
+    expect(validateUserInput("homayoun", 18)).toMatch(/success/i);
+  });
+
+  it("should return an error if username is not a string", () => {
+    expect(validateUserInput(1, 18)).toMatch(/invalid/i);
+  });
+
+  it("should return an error if username is less than 3 characters", () => {
+    expect(validateUserInput('ho', 18)).toMatch(/invalid/i);
+  });
+
+  it("should return an error if username is longer than 255 characters", () => {
+    expect(validateUserInput('H'.repeat(256), 18)).toMatch(/invalid/i);
+  });
+
+  it("should return an error if age is not a number", () => {
+    expect(validateUserInput("homayoun", "18")).toMatch(/invalid/i);
+  });
+
+  it("should return an error if age is less than 18", () => {
+    expect(validateUserInput("homayoun", 17)).toMatch(/invalid/i);
+  });
+
+  it("should return an error if age is greater than 100", () => {
+    expect(validateUserInput("homayoun", 101)).toMatch(/invalid/i);
+  });
+
+  it("should return an error if both username and age are invalid", () => {
+    expect(validateUserInput("", 0)).toMatch(/invalid username/i);
+    expect(validateUserInput("", 0)).toMatch(/invalid age/i);
   });
 });
