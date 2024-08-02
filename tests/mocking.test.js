f9790407035e764b8bb9b95f6531ebee5767fa8b
Homayoun <homayoun763@gmail.com>
2024-08-02 23:27:04 +0330

    mock example for send message

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -6,5 +6,12 @@ describe('test suite', () => {
         greet.mockImplementation(name => 'Hello' + name)
         greet('Homayoun');
         expect(greet).toHaveBeenCalledOnce('Homayoun')
+    });
+    it('should send text message', () => {
+        const sendText = vi.fn();
+        sendText.mockReturnValue('ok')
+        const result = sendText('message');
+        expect(sendText).toHaveBeenCalledWith('message');
+        expect(result).toBe('ok')
     })
 })
