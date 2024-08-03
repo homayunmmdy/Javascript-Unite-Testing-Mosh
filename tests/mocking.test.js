c0454a2555e033f98f5c4be2236b3805da5ad7f9
Homayoun <homayoun763@gmail.com>
2024-08-04 00:29:51 +0330

    Mocking Modules

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -1,4 +1,8 @@
 import {vi, it, expect, describe } from 'vitest'
+import { getPriceInCurrency } from '../src/mocking';
+import { getExchangeRate } from '../src/libs/currency';
+
+vi.mock('../src/libs/currency')
 
 describe('test suite', () => {
     it('test case', () => {
@@ -13,5 +17,13 @@ describe('test suite', () => {
         const result = sendText('message');
         expect(sendText).toHaveBeenCalledWith('message');
         expect(result).toBe('ok')
+    })
+})
+
+describe('getPriceInCurrency', () => {
+    it('should price in target currency', () => {
+        vi.mocked(getExchangeRate).mockReturnValue(1.5)
+        const price = getPriceInCurrency(10, 'AUD');
+        expect(price).toBe(15)
     })
 })
