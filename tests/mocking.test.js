c6d8fbd8707c26f098df6954a4ace64d7baba134
Homayoun <homayoun763@gmail.com>
2024-08-11 20:18:57 +0330

    setSystemTime example

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -1,5 +1,6 @@
 import { vi, it, expect, describe, beforeEach } from "vitest";
 import {
+  getDiscount,
   getPriceInCurrency,
   getShippingInfo,
   isOnline,
@@ -140,5 +141,20 @@ describe('isOnline', () => {
     expect(isOnline()).toBeTruthy()
     vi.setSystemTime('2024-01-01 19:59');
     expect(isOnline()).toBeTruthy()
+  })
+});
+
+describe('getDiscount', () => {
+  it('should return .2 on Christmas day', () => {
+    vi.setSystemTime('2024-12-25 00:01');
+    expect(getDiscount()).toBe(.2)
+    vi.setSystemTime('2024-12-25 23:59');
+    expect(getDiscount()).toBe(.2)
+  })
+  it('should return 0 on any other day', () => {
+    vi.setSystemTime('2024-12-24 00:01');
+    expect(getDiscount()).toBe(0)
+    vi.setSystemTime('2024-12-26 00:01');
+    expect(getDiscount()).toBe(0)
   })
 })
